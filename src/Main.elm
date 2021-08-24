2c020fc3983020090ae1c5216dde0212759c70b9
hasko <hasko@heinecke.com>
2021-08-25 00:45:44 +0200

    Colors and movement

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,13 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Events exposing (onAnimationFrameDelta)
 import Color exposing (Color)
-import Html exposing (div, h1, text)
+import Html exposing (div, h1, p, text)
 import Random
 import Random.Extra
-import Svg exposing (svg)
-import Svg.Attributes exposing (viewBox)
+import Svg exposing (Svg, svg)
+import Svg.Attributes as SA exposing (viewBox)
 
 
 main =
@@ -28,6 +29,7 @@ type alias Painter =
 
 type Msg
     = SeedPainters (List Painter)
+    | AnimationTick Float
 
 
 init : () -> ( Model, Cmd Msg )
@@ -35,8 +37,13 @@ init _ =
     ( Nothing, List.repeat 10 painterGenerator |> Random.Extra.sequence |> Random.generate SeedPainters )
 
 
-subscriptions _ =
-    Sub.none
+subscriptions model =
+    case model of
+        Nothing ->
+            Sub.none
+
+        Just _ ->
+            onAnimationFrameDelta AnimationTick
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,19 +52,82 @@ update msg model =
         SeedPainters painters ->
             ( Just painters, Cmd.none )
 
+        AnimationTick delta ->
+            ( movePainters delta model, Cmd.none )
+
+
+movePainters : Float -> Model -> Model
+movePainters delta model =
+    case model of
+        Nothing ->
+            Nothing
+
+        Just painters ->
+            Just (List.map (movePainter delta) painters)
+
+
+movePainter : Float -> Painter -> Painter
+movePainter delta painter =
+    let
+        d =
+            painter.step * delta / 1000.0
+
+        x =
+            wrap 0 800 (painter.px + d * cos painter.dir)
+
+        y =
+            wrap 0 600 (painter.py + d * sin painter.dir)
+    in
+    { painter | px = x, py = y }
+
+
+wrap : Float -> Float -> Float -> Float
+wrap low hi val =
+    if val < low then
+        wrap low hi (val + hi - low)
+
+    else if val > hi then
+        wrap low hi (val - hi + low)
+
+    else
+        val
+
 
 view model =
     div []
         [ h1 [] [ text "Really Nice Generative Art" ]
-        , svg [ viewBox "0 0 800 600" ] []
+        , case model of
+            Nothing ->
+                p [] [ text "Seeding" ]
+
+            Just painters ->
+                svg [ viewBox "0 0 800 600" ]
+                    (List.map painterToSvg painters)
         ]
 
 
 painterGenerator : Random.Generator Painter
 painterGenerator =
-    Random.map4
-        (\px py dir step -> Painter px py dir step (Color.rgb 1 0 0) 1.0)
+    Random.map5
+        (\px py dir step col -> Painter px py dir step col 1.0)
         (Random.float 0.0 800.0)
         (Random.float 0.0 600.0)
         (Random.float 0.0 (degrees 360))
-        (Random.float 1.0 5.0)
+        (Random.float 1.0 100.0)
+        (Random.map3
+            Color.rgb
+            (Random.float 0.0 1.0)
+            (Random.float 0.0 1.0)
+            (Random.float 0.0 1.0)
+        )
+
+
+painterToSvg : Painter -> Svg Msg
+painterToSvg painter =
+    Svg.circle
+        [ painter.px |> String.fromFloat |> SA.cx
+        , painter.py |> String.fromFloat |> SA.cy
+        , SA.r "5"
+        , SA.fill (Color.toCssString painter.color)
+        ]
+        []
